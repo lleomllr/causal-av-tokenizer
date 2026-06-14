c75d8007c7a68e3a08a3c92453853cd95b58091a
J3yROm <jerome@tipit.fr>
2026-06-14 20:46:18 +0200

    update __init__

--- src/avtokenizer/__init__.py
+++ src/avtokenizer/__init__.py
@@ -1,10 +1,12 @@
 from .data import AVWindowConfig, SynchronizedAVDataset, build_window_index, list_video_files 
 from .audio_autoencoder import AudioMelAutoencoder
+from .av_autoencoder import AudioVisualAutoencoder
 from .video_autoencoder import VideoAutoencoder
 
 __all__ = [
     "AVWindowConfig",
     "AudioMelAutoencoder",
+    "AudioVisualAutoencoder",
     "SynchronizedAVDataset",
     "VideoAutoencoder",
     "build_window_index",
